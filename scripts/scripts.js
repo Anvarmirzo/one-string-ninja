232b07fa3218caeaccccef32394cc79453a407f7
Anvarmirzo <www.latvienhoasadrizeges@gmail.com>
2022-02-06 00:14:20 +0500

    feat: #1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,31 +11,28 @@ document.addEventListener('DOMContentLoaded', function (e) {
 
 			if (!bullet || !bulletsWrapper.contains(bullet)) return;
 
-			if (bullet.hasAttribute('data-control')) {
-				// removing active state from previous slide
-				const allBullets = bulletsWrapper.querySelectorAll('[data-control]');
-				allBullets[currentSlide].classList.remove('active');
-				imageSlides[currentSlide].classList.remove('active');
-				textSlides[currentSlide].classList.remove('active');
+			// handle click on bullet
+			// removing active state from previous slide
+			const allBullets = bulletsWrapper.querySelectorAll('[data-control]');
+			allBullets[currentSlide].classList.remove('active');
+			imageSlides[currentSlide].classList.remove('active');
+			textSlides[currentSlide].classList.remove('active');
 
+			if (bullet.hasAttribute('data-control')) {
 				// updating current slide
 				currentSlide = +bullet.getAttribute('data-control');
-
-				// setting active state for current slide
-				bullet.classList.add('active');
-				imageSlides[currentSlide].classList.add('active');
-				textSlides[currentSlide].classList.add('active');
+			} else if (bullet.id === 'next-btn') {
+				// handle click on next button
+				if (currentSlide < 2) {
+					currentSlide += 1;
+				} else {
+					currentSlide = 0;
+				}
 			}
+			// setting active state for current slide
+			allBullets[currentSlide].classList.add('active');
+			imageSlides[currentSlide].classList.add('active');
+			textSlides[currentSlide].classList.add('active');
 		});
-
-		// table.onclick = function (event) {
-		// 	let td = event.target.closest('td'); // (1)
-
-		// 	if (!td) return; // (2)
-
-		// 	if (!table.contains(td)) return; // (3)
-
-		// 	highlight(td); // (4)
-		// };
 	})();
 });
